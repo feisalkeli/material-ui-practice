00450e8f5137d106ca5863082ed4e3ce7d66e28d
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-09-05 13:11:06 +0300

    final commit

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,7 +1,69 @@
 import "./App.css";
+import { cardDetails } from "./index";
+import {
+  Typography,
+  AppBar,
+  Card,
+  Button,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  CardActions,
+} from "@mui/material";
+import PhotoCameraBackIcon from "@mui/icons-material/PhotoCameraBack";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppBar position="relaative">
+        <Toolbar>
+          <PhotoCameraBackIcon color="primary" />
+          <Typography variant="h6" color="darkblue" sx={{ ml: 3 }}>
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="text-primary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Welcome to my photo Album Project built using MUI5.Feel free to
+              add recommendations.
+            </Typography>
+
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <Button variant="contained" color="primary" text="white">
+                  See My Pictures
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="primary">
+                  Edit Photo
+                </Button>
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ material-ui/src/components/index.js
@@ -0,0 +1,11 @@
+import { imgUrl } from "../assets/";
+
+export const cardDetails = [
+  {
+    id: 1,
+    title: "Heading",
+    description:
+      "Content description in less than 100 words.Manze English is Hard",
+    imgUrl: imgUrl,
+  },
+];
